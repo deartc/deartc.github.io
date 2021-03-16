88f00ca37a5a9b611d0d2e0c6e5a34efe3aab5b1
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2021-03-16 15:56:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+/*
+The code below will change
+my navigation bar from vertical to horizontal.  It is mobile first/responsive designed. :
+*/
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+/*
+The code below will show the pyrolyzer conversion from Fahrenheit to Celcius 
+and the name your enter:
+*/
+
+
+
+function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
+}
+
+
+/*
+The code below will add greet you with Hello 
+and the name your enter:
+*/
+
+function ShowHelloMessage() {
+    var name = document.getElementById("myname");
+    document.getElementById("hellomessage").innerHTML = "Hello, " + name.value;
+}
+document.getElementById("mybutton").onclick = ShowHelloMessage;
